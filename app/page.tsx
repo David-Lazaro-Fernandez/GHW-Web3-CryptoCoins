fa6dd7d1af1416164d33a80c0beae73fcc632718
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2023-09-06 04:26:20 +0000

    Get contract async function

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,15 @@ import { getWeb3 } from './Web3Setup';
 import { getContractInstance } from './ContractSetUp';
 import HelloWorldContract from '../public/contracts/HelloWorld.json';
 
-export default async function Home() {
 
+async function getContract() {
+  const web3 = await getWeb3();
+  const contractInstance = await getContractInstance(web3, HelloWorldContract);
+  const accounts = await web3.eth.getAccounts();
+  return { contractInstance, accounts };
+}
+export default async function Home() {
+  const { contractInstance, accounts } = await getContract();
   return (
     <main className="flex justify-center items-center h-screen">
      <h1></h1>
